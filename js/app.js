7353c5c858cb1d5860f3b000db5f1839d78353b9
lsjgerardo <lsjgerardo@gmail.com>
2017-01-20 09:38:23 -0600

    login

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
-var app = angular.module('fedex', ['ui.bootstrap']);
+var app = angular.module('fedex', ['ui.bootstrap','ui.router'])
+
+/*routes*/
+.config(function($stateProvider, $urlRouterProvider) {
+    console.log('state provider')
+    $stateProvider
+        .state('app', {
+            url: '/app',
+            containerClass: 'menu',
+            abstract: true,
+            templateUrl: 'views/menu.html',
+            controller: 'AppCtrl'
+        })
+        .state('login', {
+            url: '/login',
+            containerClass: 'login',
+            templateUrl: 'views/login.html',
+            controller: 'LoginCtrl'  
+            })
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/login');
+})
